372c0245899eb9c0006521a344fd6045c7d1c46a
Egbuleze Francis <francisegbuleze@gmail.com>
2024-10-22 17:26:22 +0100

    second commit

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import { UserContextProvider } from "./UserContext.jsx";
 
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
