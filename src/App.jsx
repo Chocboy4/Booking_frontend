476b18c92e0a18feb037a260c586960d82f95e7d
Egbuleze Francis <francisegbuleze@gmail.com>
2024-10-24 17:59:21 +0100

    last one

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ import BookingsPage from './pages/BookingsPage';
 import BookingPage from './pages/BookingPage';
 
 // Set Axios defaults
-axios.defaults.baseURL = 'https://booking-backend-lake.vercel.app/?vercelToolbarCode=cBclw9emGkYL1-s/api';
+axios.defaults.baseURL = 'http://localhost:4000';
 axios.defaults.withCredentials = true; // Enable sending cookies
 
 function App() {
